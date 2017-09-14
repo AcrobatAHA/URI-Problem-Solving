55375b13101e85f2da9ec7525ace08d0c12699a2
AcrobatAHA <ahanik94@gmail.com>
2017-09-14 08:20:39 +0600

    Add files via upload

---
+++ URI 1015 Distance Between Two Points in python.py
@@ -0,0 +1,11 @@
+import math
+X1,Y1 = input().split()
+X2,Y2 = input().split()
+
+X1 = float(X1)
+X2 = float(X2)
+Y1 = float(Y1)
+Y2 = float(Y2)
+
+Distance = math.sqrt(((X2-X1)*(X2-X1))+((Y2-Y1)*(Y2-Y1)))
+print("%.4f"%Distance)
