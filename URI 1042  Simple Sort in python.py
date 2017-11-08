9b4994332c078348977b198c61eb4706ec318064
AcrobatAHA <ahanik94@gmail.com>
2017-11-08 20:17:34 +0000

    Add files via upload

---
+++ URI 1042  Simple Sort in python.py
@@ -0,0 +1,14 @@
+a,b,c=input().split()
+a=int(a)
+b=int(b)
+c= int(c)
+
+print('@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+if a<b:
+    
+    print(a)
+if b<c:
+    print(b)
+if c<a:
+    print(c)
+print(a,b,c)
